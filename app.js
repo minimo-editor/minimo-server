c7d8e3e8956ee8b5a711e26a71b8b41f95fc30ba
minwooKim <vvee1253@gmail.com>
2021-05-12 17:38:33 +0900

    Add cors options

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const db = require('./configs/db');
 const app = express();
 
 app.use(logger('dev'));
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
