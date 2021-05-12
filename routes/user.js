39f9cb1656f4490bdddb3a5ecddf59b080db257f
minwooKim <vvee1253@gmail.com>
2021-05-12 14:50:05 +0900

    Rename users.js to user.js

---
+++ routes/user.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const createError = require('http-errors');
+const User = require('../models/User');
+
+router.get('/', (req, res, next) => {
+});
+
+router.post('/', async (req, res, next) => {
+  const { email, name, photoURL } = req.body;
+
+  let user;
+
+  try {
+    user = await User.findOne({ email });
+  } catch (error) {
+    // TODO: add error;
+    next(createError());
+  }
+
+  if (!user) {
+    try {
+      user = await User.create({
+        email,
+        name,
+        photoURL,
+      });
+
+    } catch (error) {
+      next(createError());
+    }
+  }
+
+  res.json({ ok: true, data: user });
+});
+
+module.exports = router;
