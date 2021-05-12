825493f8351ee9893b37a4b879e56ef251aa7097
minwooKim <vvee1253@gmail.com>
2021-05-12 17:42:24 +0900

    Add project router to index router

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-
+// NOTE: users? user? projects? project?
 const userRouter = require('./user');
+const projectRouter = require('./project');
 
 router.use('/user', userRouter);
+router.use('/project', projectRouter);
 
 module.exports = router;
