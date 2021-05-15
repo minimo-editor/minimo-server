94ffb83ca49f7377d0942b38bed1a4b54974c852
minwooKim <vvee1253@gmail.com>
2021-05-15 21:32:56 +0900

    Add error handler

--- app.js
+++ app.js
@@ -36,11 +36,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  // TODO: add error status
   res.status(err.status || 500);
-  // TODO: error handle
-  res.send();
+  res.json({ ok: false, error: err});
 });
 
 module.exports = app;
