bcc85c4280ab0cac0a116f0d7d2d498d4d34d2f7
minwooKim <vvee1253@gmail.com>
2021-05-16 10:25:09 +0900

    Add error status to json

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // TODO: add error status
-  res.status(err.status || 500);
+  err.status = err.status || 500;
   res.json({ ok: false, error: err});
 });
 
