2e8d51a826e10a517a02b75f07fe31f073333176
minwooKim <vvee1253@gmail.com>
2021-05-20 17:42:04 +0900

    Seperate db config

--- configs/db.js
+++ configs/db.js
@@ -1,17 +1,8 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URL, {
+const mongoDBConfig = {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
-});
-
-function initDb() {
-  const db = mongoose.connection;
-
-  db.on('error', (err) => console.error(err));
-  db.once('open', () => console.log('Connected to Database'));
 }
 
-exports.init = initDb;
+module.exports = mongoDBConfig;
